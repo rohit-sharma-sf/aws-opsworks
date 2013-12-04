11728a93363974cfee1d4eda276cbcab0c1da7ac
Ashish <ashishkaushik83@gmail.com>
2013-12-04 20:14:13 +0530

    Update kovelsperm.rb

--- permissions/recipes/kovelsperm.rb
+++ permissions/recipes/kovelsperm.rb
@@ -1,1 +1,9 @@
-#Add the recipe definition here. 
+script "chmod_upload" do
+  interpreter "bash"
+  user "root"
+  cwd "/"
+  code <<-EOH
+  /bin/chmod -R 777 /srv/www/kovelscom/current/tmp/
+  /bin/chmod -R 777 /srv/www/kovelscom/current/logs/
+  EOH
+end
