db755fee7c3bd6e7a59c6cd0d08e7b1cb514f06a
Ashish <ashishkaushik83@gmail.com>
2013-12-04 20:47:39 +0530

    Updated the code with creating the directory and creating symlink

--- permissions/recipes/kovelsimages.rb
+++ permissions/recipes/kovelsimages.rb
@@ -1,1 +1,10 @@
 #create directory and create the soft link here
+script "chmod_upload" do
+  interpreter "bash"
+  user "root"
+  cwd "/"
+  code <<-EOH
+  /bin/mkdir /srv/www/kovelscom/shared/images/
+  /bin/ln -s /srv/www/kovelscom/shared/images/ /srv/www/kovelscom/current/images
+  EOH
+end
